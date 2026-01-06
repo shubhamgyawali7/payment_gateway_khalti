e60d7cf27dc82211ee02049d046898deb45204ea
Shubham Gyawali <shubhamgyawali11@gmail.com>
2026-01-06 21:05:46 +0545

    update:client-server

--- client/src/api/server.js
+++ client/src/api/server.js
@@ -1,7 +1,7 @@
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.API_URL || "http://localhost:5000";
 // const KHALTI_AUTH_KEY = process.env.KHALTI_SECRET_KEY;
 const handeleKhaltiapi = async (data) => {
-  console.log("Sending Data to Server=>", data);
+  // console.log("Sending Data to Server=>", data);
   const response = await fetch(`${API_URL}/api/initiate-payment`, {
     method: "POST",
     headers: {
